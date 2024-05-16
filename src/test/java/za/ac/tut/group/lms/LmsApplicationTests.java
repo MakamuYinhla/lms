f1e217378305f11db96685541a05fd622a948c60
Yinhla <makamuyinhla2016@gmail.com>
2024-05-16 09:46:41 +0200

    First Commitre

---
+++ src/test/java/za/ac/tut/group/lms/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.tut.group.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
