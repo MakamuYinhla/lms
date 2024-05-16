c106dd1a62ded549a22ec3e60b7f0643706b4bd8
Yinhla <makamuyinhla2016@gmail.com>
2024-05-16 13:53:54 +0200

    Fixed crashing issue

--- src/main/java/za/ac/tut/group/lms/models/Student.java
+++ src/main/java/za/ac/tut/group/lms/models/Student.java
@@ -2,11 +2,27 @@ package za.ac.tut.group.lms.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "students")
 public class Student {
+    @Id
+    @GeneratedValue
+    private long UserID;
+    @Column
+    private String password;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String email;
+    @Column
+    private String role;
+
     @Column(unique = true, length = 9)
     private long studentID;
     @Column
