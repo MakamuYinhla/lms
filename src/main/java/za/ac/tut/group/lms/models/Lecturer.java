95cd042a2e4dcb4066aeb5d095798abb884f845a
FowardPro <147938121+FowardPro@users.noreply.github.com>
2024-05-16 14:27:43 +0200

    Lecture entity changed

--- src/main/java/za/ac/tut/group/lms/models/Lecturer.java
+++ src/main/java/za/ac/tut/group/lms/models/Lecturer.java
@@ -1,7 +1,26 @@
 package za.ac.tut.group.lms.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "lecture")
+public class Lecturer {
+    @Id
+    @GeneratedValue
+    private long UserID;
+    @Column
+    private String password;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String email;
+    @Column(unique = true, length = 9)
+    private long lectureID;
 
-public class Lecturer extends User{
-    
 }
