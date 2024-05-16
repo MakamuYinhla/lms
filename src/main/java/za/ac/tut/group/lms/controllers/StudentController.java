d82615c594d7aaf395f4f8bc905c01d02acff753
Lethabo <pegphina1@gmail.com>
2024-05-16 22:03:59 +0200

    Done?

--- src/main/java/za/ac/tut/group/lms/controllers/StudentController.java
+++ src/main/java/za/ac/tut/group/lms/controllers/StudentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,30 +23,15 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-<<<<<<< HEAD
-    // @GetMapping("/signup")
-    // public String showSignUpForm(){
-    //     System.out.println("Sign up was touched");
-    //     return "signup_student.html";
-    // }
-=======
     @GetMapping("/signup")
     public String showSignUpForm(){
         System.out.println("Sign up was touched");
-
-        
         return "signup_student.html";
     }
->>>>>>> 398279beda8d4064b6da8294b779f0b69d266e56
 
     @PostMapping("/addstudent")
-    public String addNewStudent(Student student, BindingResult result, Model model){
-        
-        
-        
-        
+    public void addNewStudent(Student student, BindingResult result, Model model){
         studentService.addNewStudent(student);
-        return "redirect:/courses";
     }    
 
 }
