6b60cccec4ac117e92f4018fa0d7f59f6fdce7f2
Lethabo <pegphina1@gmail.com>
2024-05-16 16:11:40 +0200

    Changed access index

--- src/main/java/za/ac/tut/group/lms/controllers/LMSController.java
+++ src/main/java/za/ac/tut/group/lms/controllers/LMSController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LMSController {
     
-    @GetMapping("/")
+    @GetMapping("index")
     public String index(){
         return "index.html";
     }
