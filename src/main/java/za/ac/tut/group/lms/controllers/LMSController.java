c3fa952b51aa06e5ded343b1b83db7f2990d6572
Lethabo <pegphina1@gmail.com>
2024-05-17 19:17:09 +0200

    Done with lecturer and student signups and sign ins

--- src/main/java/za/ac/tut/group/lms/controllers/LMSController.java
+++ src/main/java/za/ac/tut/group/lms/controllers/LMSController.java
@@ -1,13 +1,6 @@
 package za.ac.tut.group.lms.controllers;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import za.ac.tut.group.lms.models.Lecturer;
-import za.ac.tut.group.lms.models.Student;
-import za.ac.tut.group.lms.services.LecturerService;
-import za.ac.tut.group.lms.services.StudentService;
 
 
 
@@ -18,16 +11,16 @@ import za.ac.tut.group.lms.services.StudentService;
 @Controller
 public class LMSController {
     
-    @Autowired
-    private final StudentService studentService;
+    // @Autowired
+    // private final StudentService studentService;
 
-    @Autowired
-    private final LecturerService lecturerService;
+    // @Autowired
+    // private final LecturerService lecturerService;
 
-    public LMSController(StudentService studentService, LecturerService lecturerService) {
-        this.studentService = studentService;
-        this.lecturerService = lecturerService;
-    }
+    // public LMSController(StudentService studentService, LecturerService lecturerService) {
+    //     this.studentService = studentService;
+    //     this.lecturerService = lecturerService;
+    // }
 
     @GetMapping("/")
     public String index(){
@@ -55,48 +48,48 @@ public class LMSController {
     }
 
 
-    @GetMapping("/createUser")
-    public String createUser(@RequestParam String name, @RequestParam Long number, @RequestParam String email, @RequestParam String role, @RequestParam String password){
+    // @GetMapping("/createUser")
+    // public String createUser(@RequestParam String name, @RequestParam Long number, @RequestParam String email, @RequestParam String role, @RequestParam String password){
         
-        try{
-            if(role.equals("Student")){
-                //System.out.println("Student was clicked");
-                Student student = new Student();
-                student.setEmail(email);
-                student.setFirstName(name);
-                student.setPassword(password);
-                student.setStudentID(number);
-                studentService.addNewStudent(student);
-            }
-            else{
-                //System.out.println("Lecturer was clicked instead");
-                Lecturer lecturer = new Lecturer();
+    //     try{
+    //         if(role.equals("Student")){
+    //             //System.out.println("Student was clicked");
+    //             Student student = new Student();
+    //             student.setEmail(email);
+    //             student.setFirstName(name);
+    //             student.setPassword(password);
+    //             student.setStudentID(number);
+    //             studentService.addNewStudent(student);
+    //         }
+    //         else{
+    //             //System.out.println("Lecturer was clicked instead");
+    //             Lecturer lecturer = new Lecturer();
 
-                lecturer.setEmail(email);
-                lecturer.setFirstName(name);
-                lecturer.setPassword(password);
-                lecturer.setLectureID(number);
-                lecturerService.addNewLecturer(lecturer);
-            }
-        }
-        catch(Exception e){
-            return "redirect:signup";
-        }
+    //             lecturer.setEmail(email);
+    //             lecturer.setFirstName(name);
+    //             lecturer.setPassword(password);
+    //             lecturer.setLectureID(number);
+    //             lecturerService.addNewLecturer(lecturer);
+    //         }
+    //     }
+    //     catch(Exception e){
+    //         return "redirect:signup";
+    //     }
         
-        return "redirect:/";
-    }
+    //     return "redirect:/";
+    // }
     
 
-    @GetMapping("/login")
-    public String loginUser(@RequestParam String email, @RequestParam String password){
+    // @GetMapping("/login")
+    // public String loginUser(@RequestParam String email, @RequestParam String password){
 
-        try{
+    //     try{
 
-        }
-        catch(Exception e){
-            return "redirect:signup";
-        }
+    //     }
+    //     catch(Exception e){
+    //         return "redirect:signup";
+    //     }
 
-        return "redirect:/";
-    }
+    //     return "redirect:/";
+    // }
 }
--- src/main/java/za/ac/tut/group/lms/controllers/StudentController.java
+++ src/main/java/za/ac/tut/group/lms/controllers/StudentController.java
@@ -2,12 +2,9 @@ package za.ac.tut.group.lms.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-=======
->>>>>>> 72b5342d0291a24ee6f90147bbada7b5741696e2
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,7 +23,6 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-<<<<<<< HEAD
     @GetMapping("/signup")
     public String showSignUpForm(Model model){
         model.addAttribute("student",new Student());  
@@ -38,13 +34,6 @@ public class StudentController {
     // public void addNewStudent(Student student, BindingResult result, Model model){
     //     studentService.addNewStudent(student);
     // }    
-=======
-
-    @PostMapping("/addstudent")
-    public void addNewStudent(){
-        Student student = new Student();
-    }    
->>>>>>> 72b5342d0291a24ee6f90147bbada7b5741696e2
 
     @PostMapping("/addstudent")
     public String addNewStudent(@ModelAttribute Student student){
